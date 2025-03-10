5fa5e3de5cce064dadba8d3ddcece4aea4aa2111
Daniel <dfernandezsw@gmail.com>
2025-03-10 07:19:10 +0100

    Adding color to icons

--- Sources/UI/Components/TextFields/CustomTextField.swift
+++ Sources/UI/Components/TextFields/CustomTextField.swift
@@ -33,8 +33,9 @@ public struct CustomTextField: View {
                 }
                 
                 HStack(spacing: .small) {
-                    if let icon = style.icon {
+                    if let icon = style.icon, let iconColor = style.iconColor {
                         icon
+                            .foregroundStyle(iconColor)
                     }
                     
                     ZStack(alignment: .leading) {
@@ -72,14 +73,21 @@ extension CustomTextField {
         let focusedBorderColor: Color
         let placeholderColor: Color
         let icon: Image?
+        let iconColor: Color?
         let font: Font
         
-        public init(borderColor: Color, focusedBorderColor: Color, placeholderColor: Color, icon: Image?, font: Font) {
+        public init(borderColor: Color,
+                    focusedBorderColor: Color,
+                    placeholderColor: Color,
+                    font: Font,
+                    icon: Image? = nil,
+                    iconColor: Color? = nil) {
             self.borderColor = borderColor
             self.focusedBorderColor = focusedBorderColor
             self.placeholderColor = placeholderColor
-            self.icon = icon
             self.font = font
+            self.icon = icon
+            self.iconColor = iconColor
         }
     }
 }
@@ -92,7 +100,6 @@ extension CustomTextField {
                 borderColor: .gray,
                 focusedBorderColor: .red,
                 placeholderColor: .gray,
-                icon: nil,
                 font: .body
             ),
             text: .constant("Some text")
@@ -104,8 +111,21 @@ extension CustomTextField {
                 borderColor: .gray,
                 focusedBorderColor: .red,
                 placeholderColor: .gray,
-                icon: Image(systemName: "person.fill"),
-                font: .body
+                font: .body,
+                icon: Image(systemName: "person.fill")
+            ),
+            text: .constant("")
+        )
+        
+        CustomTextField(
+            placeholder: "Password",
+            style: .init(
+                borderColor: .gray,
+                focusedBorderColor: .red,
+                placeholderColor: .gray,
+                font: .body,
+                icon: Image(systemName: "lock.fill"),
+                iconColor: .red
             ),
             text: .constant("")
         )
